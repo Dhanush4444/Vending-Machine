0bb2d2571a047fe56dd37c63338e50edaa92dff5
Dhanush4444 <dhanushdazz@gmail.com>
2019-01-11 12:44:56 +0530

    Fixed logout bug

--- app/src/main/java/com/example/joker/vendingmachine/Main2Activity.java
+++ app/src/main/java/com/example/joker/vendingmachine/Main2Activity.java
@@ -1,41 +1,46 @@
 package com.example.joker.vendingmachine;
 
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.*;
-import java.lang.Long;
+
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
+
 
 public class Main2Activity extends AppCompatActivity {
+
     DatabaseReference mDatabase;
     FirebaseAuth mAuth;
-    TextView wallet,usrname;
+    TextView wallet, usrname;
     FirebaseUser mUser;
     Long walletDatal;
+    ArrayList<String> list;
     int walletData;
+
     Button logut;
-    ArrayList<Integer> prices;
+    ArrayList<Integer> prices,itemCount;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        list = new ArrayList<String>();
         wallet = (TextView) findViewById(R.id.wallet);
         mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
-        logut=(Button) findViewById(R.id.logoutButton);
-        usrname=(TextView) findViewById(R.id.usrname);
+        mUser = mAuth.getCurrentUser();
+//        values=new ArrayList<String>();
+        logut = (Button) findViewById(R.id.logoutButton);
+        usrname = (TextView) findViewById(R.id.usrname);
         final ListView listview = (ListView) findViewById(R.id.listview);
-        prices=new ArrayList<Integer>(){
+        prices = new ArrayList<Integer>() {
             {
                 add(20);
                 add(20);
@@ -50,16 +55,23 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                mUser=null;
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
             }
         });
-        String[] values = new String[]{"Dairy Milk : 20rs","Kitkat: 20rs","Lays : 10rs","Coke : 30rs","7Up : 30rs"};
+        String[] values = new String[]{"Dairy Milk : 20rs", "Kitkat: 20rs", "Lays : 10rs", "Coke : 30rs", "7Up : 30rs"};
+
+
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                wallet.setText("Wallet Balance : " + dataSnapshot.child(mUser.getUid()).child("wallet").getValue().toString().trim());
-                walletDatal=(Long) dataSnapshot.child(mUser.getUid()).child("wallet").getValue();
-                usrname.setText("Username : "+dataSnapshot.child(mUser.getUid()).child("Name").getValue().toString());
+                wallet.setText(String.format("Wallet Balance : %s", dataSnapshot.child(mUser.getUid()).child("wallet").getValue().toString().trim()));
+                walletDatal = (Long) dataSnapshot.child(mUser.getUid()).child("wallet").getValue();
+                usrname.setText(String.format("Username : %s", dataSnapshot.child(mUser.getUid()).child("Name").getValue().toString()));
+
             }
 
             @Override
@@ -67,12 +79,11 @@ public class Main2Activity extends AppCompatActivity {
 
             }
         });
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
+
+
+        list.addAll(Arrays.asList(values));
+
+
         listview.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list));
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -82,21 +93,15 @@ public class Main2Activity extends AppCompatActivity {
                                     final int position, long id) {
                 final String item = (String) parent.getItemAtPosition(position);
 
-                walletData=walletDatal.intValue();
-                if(walletData>=prices.get(position))
-                walletData-=prices.get(position);
+                walletData = walletDatal.intValue();
+                if (walletData >= prices.get(position))
+                    walletData -= prices.get(position);
                 else
                     Toast.makeText(Main2Activity.this, "Insufficient wallet balance", Toast.LENGTH_SHORT).show();
                 mDatabase.child(mUser.getUid()).child("wallet").setValue(walletData);
 
 
-
-
-
-
-
-
-                Toast.makeText(Main2Activity.this, "Purchased "+item, Toast.LENGTH_SHORT).show();
+                Toast.makeText(Main2Activity.this, "Purchased " + item, Toast.LENGTH_SHORT).show();
 
             }
 
@@ -104,32 +109,44 @@ public class Main2Activity extends AppCompatActivity {
 
     }
 
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        StableArrayAdapter(Context context, int textViewResourceId,
-                           List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-    }
-
 
 
 }
 
+//                Toast.makeText(getApplicationContext(),ob.length(),Toast.LENGTH_LONG).show();
+//                itemCoun=(Long)dataSnapshot.child("items").child("itemCount").getValue();
+//                itemCount=itemCoun.intValue();
+//                for(int i=0;i<5;i++){
+//                    list.add(dataSnapshot.child("items").child("1").child("name").getValue().toString());
+//                    list.add(dataSnapshot.child("items").child("2").child("name").getValue().toString());
+//                    list.add(dataSnapshot.child("items").child("3").child("name").getValue().toString());
+//                    list.add(dataSnapshot.child("items").child("4").child("name").getValue().toString());
+//                    list.add(dataSnapshot.child("items").child("5").child("name").getValue().toString());
+//                }
+
+
+//    private class StableArrayAdapter extends ArrayAdapter<String> {
+//
+//        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+//
+//        StableArrayAdapter(Context context, int textViewResourceId,
+//                           List<String> objects) {
+//            super(context, textViewResourceId, objects);
+//            for (int i = 0; i < objects.size(); ++i) {
+//                mIdMap.put(objects.get(i), i);
+//            }
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            String item = getItem(position);
+//            return mIdMap.get(item);
+//        }
+//
+//        @Override
+//        public boolean hasStableIds() {
+//            return true;
+//        }
+//
+//    }
+//
